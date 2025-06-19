8971a9039971f33fde5d3f117c98f78b25593af8
[Luken Larrea] <[luken.larrea@alumni.mondragon.edu]>
2025-06-19 12:07:23 +0200

    Imputamos con KNN los NAs

--- ExamenReto4.R
+++ ExamenReto4.R
@@ -9,3 +9,26 @@ library(readxl)
 library(smoothmest)
 library(VIM)
 library(plumber)
+
+datos <- read.csv("netflix.csv")
+
+#Data Discovering
+exploracion <- datos[,10:11]
+summary(exploracion)
+sd(exploracion$Minutes,na.rm = T)
+sd(exploracion$Seasons,na.rm = T)
+
+sum(table(unique(datos$type)))
+sum(table(unique(datos$country)))
+sum(table(unique(datos$director)))
+
+#Data Cleaning
+miss_case_summary(datos)
+miss_var_summary(datos)
+
+vis_miss(datos)
+
+#Tratamiento de NAS
+KNN <- kNN(datos,variable = c("Minutes","Seasons"))
+countNA(KNN$Minutes)
+countNA(KNN$Seasons)
