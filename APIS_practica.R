cd33ac7b420cb86ee98c969ab24375a23a754d2a
[Luken Larrea] <[luken.larrea@alumni.mondragon.edu]>
2025-06-19 15:47:22 +0200

    Tercer ejercicio APIS practica

--- APIS_practica.R
+++ APIS_practica.R
@@ -2,20 +2,17 @@ library(plumber)
 library(dplyr)
 library(ggplot2)
 
-#* @apiTitle Filtrados duracion peliculas
-#* @apiDescription El usuario elige minutos y se le devuelven peliculas con menos minutos de los elegidos
-#* @param min_duracion Tipo de duracion de la pelicula
+#* @apiTitle API para devolver los n titulos mas largos
+#* @apiDescription El usuario elige cuantas peliculas quiere ver y se devuelven las mas largas
+#* @param cantidad Peliculas a devolver
 #* @get /datos_filtrados
 
-
-datos_filtrados <- function(min_duracion = 90) {
+datos_filtrados <- function(cantidad=10){
   datos <- read.csv("datosKNN.csv")
+  cantidad <- as.numeric(cantidad)
   datos$Minutes <- as.numeric(datos$Minutes)
-  datos$title <- as.character(datos$title)
-  min_duracion <- as.numeric(min_duracion)
   
-  datos_nuevos <- datos %>% filter(Minutes > min_duracion) %>% select(title)
-  
-  return(datos_nuevos)
+  peliculas <- datos %>% arrange(desc(datos$Minutes)) %>% select(Minutes,title) 
+  return(head(peliculas,cantidad))
 }
 
